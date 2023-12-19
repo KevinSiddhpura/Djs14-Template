e64bd1709c3769a59c6bd7f2c94dd17616866b5b
Kevin Siddhpura <kevinsiddhpura100@gmail.com>
2023-12-19 13:00:47 +0530

    Added interval to trigger timedMod

--- src/events/ready/02-isReady.js
+++ src/events/ready/02-isReady.js
@@ -1,6 +1,16 @@
 const chalk = require("chalk");
-const logger = require("../../modules/logger")
+const logger = require("../../modules/logger");
+const timedMod = require("../../modules/handlers/timedMod");
+const ms = require("ms");
 
 module.exports = (client) => {
     logger.info(`Logged in as ${chalk.bold.underline(client.user.username)}!`);
+
+    setTimeout(() => {
+        timedMod().then(() => {
+            setInterval(() => {
+                timedMod()
+            }, ms("2m"));
+        })
+    }, ms("10s"));
 }
