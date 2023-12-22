d13d341accef4864e9d728fc2d6917811ebf9f65
Kevin Siddhpura <kevinsiddhpura100@gmail.com>
2023-12-22 22:11:09 +0530

    Unmute and Unban updated

--- src/commands/moderation/unban.js
+++ src/commands/moderation/unban.js
@@ -1,4 +1,5 @@
 const { ApplicationCommandOptionType, Client, CommandInteraction } = require("discord.js");
+const { getDatabase } = require("../../modules/handlers/database");
 
 module.exports = {
     name: "unban",
@@ -9,19 +10,36 @@ module.exports = {
     roleRequired: ["Admin"],
     options: [
         {
-            type: ApplicationCommandOptionType.User,
-            name: "member",
-            description: "Mention the user to unban",
+            type: ApplicationCommandOptionType.String,
+            name: "user",
+            description: "Provide a user id to unban",
             required: true,
         },
-        {
-            type: ApplicationCommandOptionType.String,
-            name: "reason",
-            description: "Reason for the unban",
-            required: false,
-        }
     ],
     execute: async (/**@type {Client} */ client, /**@type {CommandInteraction} */ interaction) => {
-        
+        await interaction.deferReply({ ephemeral: true });
+
+        const userId = interaction.options.getString("user");
+        const db = getDatabase("infractions");
+        const bannedUsers = await interaction.guild.bans.fetch();
+
+        const data = await db.findOne({
+            where: {
+                userId: userId,
+            },
+        });
+
+        if (data && data.currentBan) {
+            await data.update({
+                currentBan: JSON.stringify([]),
+            });
+        };
+
+        if (!bannedUsers.has(userId)) {
+            return interaction.editReply("User is not banned");
+        } else {
+            await interaction.guild.bans.remove(userId);
+            return interaction.editReply("User has been unbanned");
+        };
     }
 }
--- src/commands/moderation/unmute.js
+++ src/commands/moderation/unmute.js
@@ -1,4 +1,7 @@
 const { ApplicationCommandOptionType, Client, CommandInteraction } = require("discord.js");
+const { getDatabase } = require("../../modules/handlers/database");
+const { getRole } = require("../../modules/utils");
+const { config } = require("../..");
 
 module.exports = {
     name: "unmute",
@@ -11,17 +14,45 @@ module.exports = {
         {
             type: ApplicationCommandOptionType.User,
             name: "member",
-            description: "Mention the user to unmute",
+            description: "Provide a member unmute",
             required: true,
         },
-        {
-            type: ApplicationCommandOptionType.String,
-            name: "reason",
-            description: "Reason for the unmute",
-            required: false,
-        }
     ],
     execute: async (/**@type {Client} */ client, /**@type {CommandInteraction} */ interaction) => {
+        await interaction.deferReply({ ephemeral: true });
+
+        const user = interaction.options.getUser("member");
+        const member = await interaction.guild.members.fetch(user.id);
+
+        if (!member) {
+            return interaction.editReply("User not found in server");
+        };
+
+        const role = await getRole(config.mutedRole, interaction.guild);
+        if(!role) {
+            return interaction.editReply("Muted role not found");
+        }
+
+        const db = getDatabase("infractions");
+
+        const data = await db.findOne({
+            where: {
+                userId: member.id
+            },
+        });
         
+        if(data && data.currentMute.length > 0) {
+            await data.update({
+                currentMute: JSON.stringify([]),
+            });
+        };
+
+        try {
+            await member.roles.remove(role.id);
+        } catch (e) {
+
+        }
+
+        return interaction.editReply("User has been unmuted");
     }
 }
