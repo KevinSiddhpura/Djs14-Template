2bb2c9353ae8108fa2077bc6a0cf704c6b36df1f
Kevin Siddhpura <kevinsiddhpura100@gmail.com>
2023-12-26 13:15:37 +0530

    Join Roles Support

---
+++ src/events/guildMemberRemove/saveRoles.js
@@ -0,0 +1,29 @@
+const { Client, GuildMember } = require("discord.js");
+const { getDatabase } = require("../../modules/handlers/database");
+const { getRole } = require("../../modules/utils");
+const { config } = require("../..");
+const logger = require("../../modules/logger");
+
+module.exports = async ( /**@type {Client} */ client, /**@type {GuildMember} */ member) => {
+    if (!config.userJoinRoles.enabled) return;
+
+    const guild = member.guild;
+    const db = getDatabase("savedRoles");
+    const userRoles = member.roles.cache.map(role => role.id);
+
+    const [data, created] = await db.findOrCreate({
+        where: {
+            user: member.id
+        },
+        defaults: {
+            user: member.id,
+            roles: JSON.stringify(userRoles)
+        }
+    });
+
+    if (data) {
+        await data.update({
+            roles: JSON.stringify(userRoles)
+        });
+    }
+}
