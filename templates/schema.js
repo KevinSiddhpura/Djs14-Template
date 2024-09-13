f9e2344fb96e8ed2c99669a96105c935a8908de6
KevinSidd <siddhpurakevin@gmail.com>
2024-09-13 23:47:44 +0530

    fix: Updated the schema template to newer one

--- templates/schema.js
+++ templates/schema.js
@@ -1,12 +1,21 @@
-// Must create this under models folder
-// Name of the file is the name of the model
+const mongoose = require("mongoose");
 
-const { Schema } = require("mongoose");
-
-module.exports = new Schema({
+const testSchema = new mongoose.Schema({
     guildID: {
+        type: String
+    },
+    userID: {
+        type: String
+    },
+    channelID: {
+        type: String
+    },
+    messageID: {
         type: String
     },
 }, {
     timestamps: true,
 });
+
+const testModal = mongoose.model("test", testSchema);
+module.exports = testModal;
