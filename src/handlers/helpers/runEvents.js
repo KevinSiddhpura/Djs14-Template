5b55d16beb039b9f14ebadffb5f02e66221f01bd
KevinSiddhpura <siddhpurakevin@gmail.com>
2024-10-21 23:22:53 +0530

    fix: using Utils

--- src/handlers/helpers/runEvents.js
+++ src/handlers/helpers/runEvents.js
@@ -1,18 +1,18 @@
-const { getFiles } = require("../utils");
+const Utils = require("../utils");
 const path = require("path");
 
 module.exports = async (client) => {
-    const eventFolders = getFiles(path.join(__dirname, "../../events"), true);
-    for(const folder of eventFolders) {
+    const eventFolders = Utils.getFiles(path.join(__dirname, "../../events"), true);
+    for (const folder of eventFolders) {
         const eventName = folder.split(path.sep).pop().split(".")[0];
 
-        const files = getFiles(folder);
+        const files = Utils.getFiles(folder);
         files.sort((a, b) => b - a);
 
-        for(const file of files) {
-            if(!file.endsWith(".js")) continue;
+        for (const file of files) {
+            if (!file.endsWith(".js")) continue;
             const event = require(file);
-            if(event.once) {
+            if (event.once) {
                 client.once(eventName, (...args) => event.run(client, ...args));
             } else {
                 client.on(eventName, (...args) => event.run(client, ...args));
